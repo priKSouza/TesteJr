acabebccc592c25f3e90d55c320ebd94a1656440
priKSouza <priscilakarinajump@gmail.com>
2021-11-01 08:50:34 -0300

    Nomeando os pokemons :dancers:

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pokemon } from '../types/pokemon.types';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  pokemons = [];
+  pokemons: Pokemon[] = [];
+  pokemonsFiltrados: Pokemon[] = [];
 
   constructor(private httpClient: HttpClient) {
     this.carregarPokemons();
@@ -19,4 +21,12 @@ export class PokemonService {
 
     this.pokemons = requisicao.results;
   }
+
+  async carregarPokemonsFiltrados(nome: string) {
+    const listaPokemonsFiltrados = await this.httpClient
+      .get<any>(`https://pokeapi.co/api/v2/pokemon/${nome}/`)
+      .toPromise();
+
+    this.pokemonsFiltrados = listaPokemonsFiltrados.results;
+}
 }
